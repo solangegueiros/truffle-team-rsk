fd8aff8b592c17eb4f17133caa57b2dbd50205d0
Solange Gueiros <solangegueiros@gmail.com>
2020-05-28 01:06:23 -0300

    comment configs

--- truffle-config.js
+++ truffle-config.js
@@ -1,22 +1,22 @@
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
 
-  const HDWalletProvider = require('@truffle/hdwallet-provider');
+  // const HDWalletProvider = require('@truffle/hdwallet-provider');
 
-  const fs = require('fs');
-  const mnemonic = fs.readFileSync(".secret").toString().trim();
-   if (!mnemonic || mnemonic.split(' ').length !== 12) {
-    // throw new Error('unable to retrieve mnemonic from .secret');
-    console.log('unable to retrieve mnemonic from .secret');
-  }
+  // const fs = require('fs');
+  // const mnemonic = fs.readFileSync(".secret").toString().trim();
+  //  if (!mnemonic || mnemonic.split(' ').length !== 12) {
+  //   // throw new Error('unable to retrieve mnemonic from .secret');
+  //   console.log('unable to retrieve mnemonic from .secret');
+  // }
   
-  const gasPriceTestnetRaw = fs.readFileSync(".gas-price-testnet.json").toString().trim();
-  const gasPriceTestnet = parseInt(JSON.parse(gasPriceTestnetRaw).result, 16);
-  if (typeof gasPriceTestnet !== 'number' || isNaN(gasPriceTestnet)) {
-    //throw new Error('unable to retrieve network gas price from .gas-price-testnet.json');
-    console.log('unable to retrieve network gas price from .gas-price-testnet.json');
-  }
-  console.log("Gas price Testnet: " + gasPriceTestnet);
+  // const gasPriceTestnetRaw = fs.readFileSync(".gas-price-testnet.json").toString().trim();
+  // const gasPriceTestnet = parseInt(JSON.parse(gasPriceTestnetRaw).result, 16);
+  // if (typeof gasPriceTestnet !== 'number' || isNaN(gasPriceTestnet)) {
+  //   //throw new Error('unable to retrieve network gas price from .gas-price-testnet.json');
+  //   console.log('unable to retrieve network gas price from .gas-price-testnet.json');
+  // }
+  // console.log("Gas price Testnet: " + gasPriceTestnet);
   
   module.exports = {
     networks: {
