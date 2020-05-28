cbe052fb6f3b4f135faccaf0547efcb02d639e26
Solange Gueiros <solangegueiros@gmail.com>
2020-05-28 10:46:10 -0300

    event

--- contracts/Register.sol
+++ contracts/Register.sol
@@ -3,8 +3,11 @@ pragma solidity 0.5.4;
 contract Register {
     string private info;
 
+    event InfoChange(string oldInfo, string newInfo);
+
     function setInfo(string memory _info) public {
         info = _info;
+        emit InfoChange (info, _info);
     }
     
     function getInfo() public view returns (string memory) {
